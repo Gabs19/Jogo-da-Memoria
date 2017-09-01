06d934a056fa131c6f960dde70988f7a97cac2da
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2017-09-01 01:23:52 -0300

    alterando imagem ao clicar no imageButton

--- app/src/main/java/com/example/god/jogodamemoria/Game.java
+++ app/src/main/java/com/example/god/jogodamemoria/Game.java
@@ -8,14 +8,20 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.logging.Logger;
 
+import static com.example.god.jogodamemoria.R.id.grid;
+
 
 public class Game extends AppCompatActivity implements View.OnClickListener {
 
-    private Button[][] imgBtn;
+    private ImageButton[][] imgBtn;
+    private ImageButton btn;
+    private GridLayout grid;
+
     /**
      * @param savedInstanceState
      */
@@ -23,39 +29,34 @@ public class Game extends AppCompatActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        //
-        GridLayout grid = (GridLayout) findViewById(R.id.grid);
+
+        grid = (GridLayout) findViewById(R.id.grid);
         grid.setColumnCount(4);
-        imgBtn = new Button[2][4];
+        imgBtn = new ImageButton[2][4];
 
         for (int i = 0; i < imgBtn.length; i++) {
-           for (int j = 0; j < imgBtn[i].length; j++) {
+            for (int j = 0; j < imgBtn[i].length; j++) {
 
+                btn = new ImageButton(this);
+                btn.setImageResource(R.mipmap.ic_launcher);
+                imgBtn[i][j] = btn;
+                grid.addView(btn);
+                btn.setOnClickListener(this);
 
-            Button btn = new Button(getApplicationContext());
-            btn.setText("" + i + "," + j);
-            imgBtn[i][j] = btn;
-            grid.addView(btn);
-            btn.setOnClickListener(this);
-
-                /*View v = grid.getChildAt(i);
-
-                if (v instanceof ImageButton) {
-                    ImageButton ib = new ImageButton(this);
-                    // adicionar botão no grid
-                    //                    grid.
-//                                grid.add
-                    imgBtn[i][j] = (ImageButton) grid.getChildAt(i);
-                }*/
             }
         }
     }
+
     @Override
     public void onClick(View v) {
-        if (v instanceof Button) {
-            CharSequence text = ((Button) v).getText();
-            Log.d("CARD", text.toString());
-            Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+        if (v instanceof ImageButton) {
+            for (int i = 0; i < imgBtn.length; i++) {
+                for (int j = 0; j < imgBtn[i].length; j++) {
+                    if (imgBtn[i][j].isPressed()) {
+                        imgBtn[i][j].setImageResource(R.mipmap.ic_launcher_round);
+                    }
+                }
+            }
         }
     }
 }
