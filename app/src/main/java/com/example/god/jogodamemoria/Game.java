09e55285e1c840d94d2a9e1cd165a483f69c927e
Gabriel Lima <mugiwara.gl@gmail.com>
2017-10-03 19:06:52 -0300

    Jogo funcional

--- app/src/main/java/com/example/god/jogodamemoria/Game.java
+++ app/src/main/java/com/example/god/jogodamemoria/Game.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.util.logging.Logger;
@@ -22,6 +24,9 @@ public class Game extends AppCompatActivity implements View.OnClickListener {
     private ImageButton btn;
     private GridLayout grid;
     private CardInfo [][] cardInfo;
+    private ImageButton selected;
+
+    public int count = 0;
 
     /**
      * @param savedInstanceState
@@ -34,16 +39,20 @@ public class Game extends AppCompatActivity implements View.OnClickListener {
         grid = (GridLayout) findViewById(R.id.grid);
         grid.setColumnCount(4);
         imgBtn = new ImageButton[2][4];
+        cardInfo = new CardInfo[2][4];
 
         for (int i = 0; i < imgBtn.length; i++) {
             for (int j = 0; j < imgBtn[i].length; j++) {
 
                 btn = new ImageButton(this);
                 btn.setImageResource(R.mipmap.ic_launcher);
+                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150,150);
+                btn.setLayoutParams(layoutParams);
                 imgBtn[i][j] = btn;
                 grid.addView(btn);
                 btn.setOnClickListener(this);
-//                btn.setTag(cardInfo);
+                cardInfo[i][j] = new CardInfo(j, R.mipmap.ic_launcher);
+                btn.setTag(cardInfo[i][j]);
             }
         }
     }
@@ -52,15 +61,28 @@ public class Game extends AppCompatActivity implements View.OnClickListener {
     public void onClick(View v) {
         if (v instanceof ImageButton) {
             ImageButton button = (ImageButton) v;
-            button.setImageResource(R.mipmap.ic_launcher_round);
-//            Log.d("JOGO", button.getTag(cardInfo).toString());
-            /*for (int i = 0; i < imgBtn.length; i++) {
-                for (int j = 0; j < imgBtn[i].length; j++) {
-                    if (imgBtn[i][j].isPressed()) {
-                        imgBtn[i][j].setImageResource(R.mipmap.ic_launcher_round);
+
+            if(count == 0){
+                button.setImageResource(R.mipmap.ic_launcher_round);
+                button.setEnabled(false);
+                selected = button;
+                count++;
+            }
+        else if (count == 1){
+                if (selected != button) {
+                    CardInfo second = (CardInfo) button.getTag();
+                    if (((CardInfo) selected.getTag()).getType() == second.getType()) {
+                        button.setImageResource(R.mipmap.ic_launcher_round);
+                        button.setEnabled(false);
+                    } else {
+                        selected.setImageResource(R.mipmap.ic_launcher);
+                        selected.setEnabled(true);
                     }
                 }
-            }*/
+
+                count = 0;
+                selected = null;
+            }
         }
     }
 
