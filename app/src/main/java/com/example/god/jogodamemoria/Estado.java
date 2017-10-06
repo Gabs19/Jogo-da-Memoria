8b44ed6710ccbb569c73096149c661f89cc2b1c1
Gabriel Lima <mugiwara.gl@gmail.com>
2017-10-06 15:51:47 -0300

    Enum

---
+++ app/src/main/java/com/example/god/jogodamemoria/Estado.java
@@ -0,0 +1,9 @@
+package com.example.god.jogodamemoria;
+
+/**
+ * Created by Gabriel on 05/10/2017.
+ */
+public enum Estado {
+    VIRADA, NAO_VIRADA;
+
+}
--- app/src/main/java/com/example/god/jogodamemoria/Game.java
+++ app/src/main/java/com/example/god/jogodamemoria/Game.java
@@ -2,31 +2,20 @@ package com.example.god.jogodamemoria;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
-
-import java.util.logging.Logger;
-
-import static com.example.god.jogodamemoria.R.id.grid;
-
 
 public class Game extends AppCompatActivity implements View.OnClickListener {
 
     private ImageButton[][] imgBtn;
+
     private ImageButton btn;
     private GridLayout grid;
-    private CardInfo [][] cardInfo;
+    private CardInfo[][] cardInfo;
     private ImageButton selected;
-
-    public int count = 0;
+    public Estado estado;
 
     /**
      * @param savedInstanceState
@@ -36,6 +25,7 @@ public class Game extends AppCompatActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        estado = Estado.NAO_VIRADA;
         grid = (GridLayout) findViewById(R.id.grid);
         grid.setColumnCount(4);
         imgBtn = new ImageButton[2][4];
@@ -46,7 +36,7 @@ public class Game extends AppCompatActivity implements View.OnClickListener {
 
                 btn = new ImageButton(this);
                 btn.setImageResource(R.mipmap.ic_launcher);
-                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150,150);
+                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
                 btn.setLayoutParams(layoutParams);
                 imgBtn[i][j] = btn;
                 grid.addView(btn);
@@ -62,13 +52,12 @@ public class Game extends AppCompatActivity implements View.OnClickListener {
         if (v instanceof ImageButton) {
             ImageButton button = (ImageButton) v;
 
-            if(count == 0){
+            if (estado == Estado.NAO_VIRADA) {
                 button.setImageResource(R.mipmap.ic_launcher_round);
                 button.setEnabled(false);
                 selected = button;
-                count++;
-            }
-        else if (count == 1){
+                estado = Estado.VIRADA;
+            } else if (estado == Estado.VIRADA) {
                 if (selected != button) {
                     CardInfo second = (CardInfo) button.getTag();
                     if (((CardInfo) selected.getTag()).getType() == second.getType()) {
@@ -80,7 +69,7 @@ public class Game extends AppCompatActivity implements View.OnClickListener {
                     }
                 }
 
-                count = 0;
+                estado = Estado.NAO_VIRADA;
                 selected = null;
             }
         }
